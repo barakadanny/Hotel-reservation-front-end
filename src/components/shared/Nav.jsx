527d730f0f101790bb5b2693c06ef5b0343ef058
Danny <barakadan421@gmail.com>
2023-05-18 22:24:50 +0300

    Update Nav

--- src/components/shared/Nav.jsx
+++ src/components/shared/Nav.jsx
@@ -25,8 +25,7 @@ function Nav() {
         };
       }, [showShadow]);
   return (
-    <header className={`${showShadow ? 'drop-shadow-md' : ''} bg-opacity-30z-30 bg-transparent fixed left-0 right-0 z-30`}>
-    {/* <header className={`${showShadow ? 'drop-shadow-md' : ''} bg-opacity-30z-30 bg-[#101212] fixed left-0 right-0 z-30`}> */}
+    <header className={`${showShadow ? 'drop-shadow-md bg-primaryColor' : ''} bg-opacity-30z-30 bg-transparent fixed left-0 right-0 z-30`}>
         <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-16 lg:h-20">
                 <div className="flex-shrink-0">
@@ -36,15 +35,15 @@ function Nav() {
                 </div>
 
                 <div
-                    className={`flex bg-[#101212] drop-shadow-xl flex-col mt-[22rem] px-24 py-5 space-y-5   ${
+                    className={`flex bg-primaryColor md:bg-transparent drop-shadow-xl flex-col mt-[17rem] px-24 py-5 space-y-5   ${
                     isOpen ? 'translate-y-0' : '-translate-y-96'
-                    } duration-1000 ease-in-out lg:drop-shadow-none lg:translate-y-0 lg:bg-[#101212] lg:space-y-0 lg:py-0  lg:px-0 lg:flex-row lg:mt-0 lg:items-center lg:justify-center lg:space-x-10`}
+                    } duration-1000 ease-in-out lg:drop-shadow-none lg:translate-y-0 lg:space-y-0 lg:py-0  lg:px-0 lg:flex-row lg:mt-0 lg:items-center lg:justify-center lg:space-x-10`}
                 >
                     <a href="." title="" className="text-base text-white transition-all duration-200 hover:text-opacity-70"> About </a>
 
                     <a href="." title="" className="text-base text-white transition-all duration-200 hover:text-opacity-70"> Ratings </a>
 
-                    <select className='text-base text-white outline-none cursor-pointer bg-[#101212] transition-all duration-200 hover:text-opacity-70' name="language" id="language">
+                    <select className='text-base text-white outline-none cursor-pointer bg-primaryColor md:bg-transparent transition-all duration-200 hover:text-opacity-70' name="language" id="language">
                         <option value="EN">English</option>
                         <option value="IN">Indi</option>
                         <option value="FR">French</option>
