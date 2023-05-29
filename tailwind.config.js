8e52960dcdb6d03a7d403faefa41e31471fdf460
Danny <barakadan421@gmail.com>
2023-05-29 13:46:26 +0300

    Add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ module.exports = {
     extend: {
       colors: {
         primaryColor: '#10B981',
+        primaryColorRgba: 'rgb(16, 185, 129, 0.2)',
         textColor: '#1b1c57',
+        redColor: '#EF4444',
+        redBgColor: '#FEE2E2',
         smallTextColor: '#f59e0b',
         paragraphColor: '#626687',
         addressColor: '#888B97',
-        customerNameColor: '#0E1735'
+        customerNameColor: '#0E1735',
+        customGray100: '#888B97',
+        customGray100Rgba: 'rgb(136, 139, 151, 0.2)'
       }
     },
   },
