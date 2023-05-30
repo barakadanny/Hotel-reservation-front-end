be70329bc6def0d23da8b1e26e1d33d16377edfa
Danny <barakadan421@gmail.com>
2023-05-30 13:49:15 +0300

    Featured component

---
+++ src/components/home/Featured.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tab from './featuredcomponents/Tab'
+
+function Featured() {
+  return (
+    <div className='px-4 py-4'>
+        <Tab/>
+    </div>
+  )
+}
+
+export default Featured
