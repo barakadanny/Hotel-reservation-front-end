8063090f223600e9664e39bfd6b9f7bc8407aa57
Danny <barakadan421@gmail.com>
2023-05-30 13:53:58 +0300

    Update Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Featured from '../components/home/Featured'
 import Hero from '../components/home/Hero'
+import Tour from '../components/home/Tour'
+import Testimony from '../components/home/Testimony'
+import Articles from '../components/home/Articles'
 
 function Home() {
   return (
     <div>
       <Hero />
+      <Featured/>
+      <Tour/>
+      {/* <Testimony/> */}
+      <Articles/>
     </div>
   )
 }
